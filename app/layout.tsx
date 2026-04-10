ab2358e7f51eb0d43da01617d665bbbddeae117c
andrijazz <a.djurisic@yahoo.com>
2026-04-11 01:10:35 +0200

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Welcome",
   description: "Personal site and blog",
+  icons: {
+    icon: "/icon.png",
+  },
 };
 
 export default function RootLayout({
